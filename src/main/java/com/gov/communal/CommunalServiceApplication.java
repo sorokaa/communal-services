0d93e8108b00095a68dbeaff642dc73391434f72
Oleksandr Soroka <sasha20999@outlook.com>
2023-10-22 15:50:12 +0300

    init project

---
+++ src/main/java/com/gov/communal/CommunalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gov.communal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunalServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gov/communalservice/CommunalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gov.communal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
